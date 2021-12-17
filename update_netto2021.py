6bae1ecaa65bd7b6d3b2cc147bb71f9b310c082e
Gengsu07 <82944512+Gengsu07@users.noreply.github.com>
2021-12-17 14:24:50 +0700

    gitignore

--- update_netto2021.py
+++ update_netto2021.py
@@ -1,7 +1,9 @@
 
+from mysql.connector import connection
 import pandas as pd
 from selenium import webdriver
 from sqlalchemy import create_engine
+from sqlalchemy import *
 import streamlit as st 
 from hydralit import HydraHeadApp
 from streamlit.legacy_caching.caching import _write_to_cache
@@ -16,8 +18,9 @@ class update_db(HydraHeadApp):
         self.title = title
 
     def run(self):
-        db_conn = create_engine('mysql://sugengw07:sgwi2341@127.0.0.1:3306/mpninfo')
-        kueri = '''
+      db_conn = create_engine('mysql://sugengw07:sgwi2341@127.0.0.1:3306/mpninfo')
+      connection = db_conn.raw_connection()
+      kueri = '''
         SELECT admin,
        npwp,
        kpp,
@@ -138,60 +141,60 @@ class update_db(HydraHeadApp):
       WHERE YEAR(TANGGALDOC) = '2021'
       AND   A.KPP2 = B.KPP
       AND   A.CABANG2 = B.CABANG;'''
-        def update(x):
-            netto2021 = pd.read_sql(x,db_conn)
-            netto2021['FULL'] = netto2021.npwp+netto2021.kpp+netto2021.cabang
-            netto2021['FULL'] = netto2021['FULL'].astype('str')
+      def update(x):
+        netto2021 = pd.read_sql(x,db_conn)
+        netto2021['FULL'] = netto2021.npwp+netto2021.kpp+netto2021.cabang
+        netto2021['FULL'] = netto2021['FULL'].astype('str')
 
-            netto2021.drop(['admin', 'npwp', 'kpp', 'cabang'],axis=1,inplace=True)
-            kestring = ['tanggalbayar','bulanbayar', 'tahunbayar'] 
-            for i in kestring:
-                netto2021[i] = netto2021[i].astype('str')
+        netto2021.drop(['admin', 'npwp', 'kpp', 'cabang'],axis=1,inplace=True)
+        kestring = ['tanggalbayar','bulanbayar', 'tahunbayar'] 
+        for i in kestring:
+            netto2021[i] = netto2021[i].astype('str')
 
-            mf = pd.read_excel(r'F:\STREAMLIT\DASHTEAM\MFWP\MFWP+klu.xlsx',
-            usecols=['NPWP','NAMA_WP','NAMA_AR','SEKSI','NAMA_KLU'],
-            dtype={'NPWP':'str'},engine='openpyxl')
+        mf = pd.read_excel(r'F:\STREAMLIT\DASHTEAM\MFWP\MFWP+klu.xlsx',
+        usecols=['NPWP','NAMA_WP','NAMA_AR','SEKSI','NAMA_KLU'],
+        dtype={'NPWP':'str'},engine='openpyxl')
 
-            kdmap = pd.read_excel('D:\DATA KANTOR\MASTERFILE\KODE_MAP_FILTERED.xlsx', dtype={'KD MAP':'str'})
-            netto2021 = pd.merge(netto2021,mf,left_on='FULL',right_on='NPWP',how='left')
-            netto2021 = pd.merge(netto2021,kdmap,left_on='kdmap',right_on='KD MAP',how='left')
-            netto2021.drop('kdmap',axis=1,inplace=True)
+        kdmap = pd.read_excel('D:\DATA KANTOR\MASTERFILE\KODE_MAP_FILTERED.xlsx', dtype={'KD MAP':'str'})
+        netto2021 = pd.merge(netto2021,mf,left_on='FULL',right_on='NPWP',how='left')
+        netto2021 = pd.merge(netto2021,kdmap,left_on='kdmap',right_on='KD MAP',how='left')
+        netto2021.drop('kdmap',axis=1,inplace=True)
 
-            kosong = ['NAMA_WP', 'NAMA_AR', 'SEKSI', 'NAMA_KLU']
-            for i in kosong:
-                netto2021[i].fillna('Non WP Madtim',inplace=True)
+        kosong = ['NAMA_WP', 'NAMA_AR', 'SEKSI', 'NAMA_KLU']
+        for i in kosong:
+            netto2021[i].fillna('Non WP Madtim',inplace=True)
 
-            netto2021.to_sql(con=db_conn,name='netto2021',if_exists='replace',index=False)
+        netto2021.to_sql(con=db_conn,name='netto2021',if_exists='replace',index=False)
 
-        def update_rank():
-          chrome_options = Options()
-          chrome_options.headless = True
-          browser = webdriver.Chrome(options=chrome_options)
-          browser.get('http://appportal/login/')
-          username = browser.find_element_by_class_name('username')
-          username.send_keys('810202558')
-          password = browser.find_element_by_class_name('password')
-          password.send_keys('Gengsu07')
-          browser.find_element_by_class_name('button_green').send_keys(Keys.ENTER)
-          objek = browser.find_element_by_xpath('//*[@id="control"]/text()')
-          print(objek.text)
-
-        st.markdown('___')
+      def capaian():
+        #Penerimaan
+        @st.cache(allow_output_mutation=True)
+        def netto21(x):
+          data = pd.read_sql(x,db_conn)
+          return data
+          
+        netto2021 = netto21('select sum(nominal) as netto from netto2021;')['netto'].sum()
+        capaian = (netto2021/10171068857000)*100
+        
+        data = pd.DataFrame(capaian)
+        data.to_sql(con=connection,name='capaian2021',if_exists='replace',index=False)
        
-        st.title('Update Data Netto')
-        if st.button('Update Sini hu',key=1):
-            st.warning('Silakan Tunggu')
-            update(kueri)
-            st.success('Sukses hu')
-        st.title('Update Data Ranking')
-        if st.button('Update Sini hu',key=2):
-            st.warning('Silakan Tunggu')
-            rank = update_rank()
-            st.write(rank)
-            st.success('Sukses hu')
+
+      st.markdown('___')
+      
+      st.title('Update Data Netto')
+      if st.button('Update Sini hu',key=1):
+        st.warning('Silakan Tunggu')
+        update(kueri)
+        st.success('Sukses hu')
+      st.title('Update Data Capaian')
+      if st.button('Update Sini hu',key=2):
+        st.warning('Silakan Tunggu')
+        capaian = capaian()
+        st.success('Sukses hu')
           
         
-        return super().run()
+      return super().run()
 
 
 
